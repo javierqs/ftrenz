c6507190c4ddcc0572ae890333e37d4e4f056e7c
Javier <jquarites@gmail.com>
2012-07-12 23:13:56 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ftrenz::Application.config.secret_token = 'd830877c66534217c99d7d6a7fa67fa0462cc4f45a3cb58f780a05baedc0783369d1443e2aad7bec0c3245fd0b2bc21014106d95025947e88fc093338974726a'
